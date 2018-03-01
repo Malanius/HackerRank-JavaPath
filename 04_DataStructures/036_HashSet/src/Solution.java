417e6574beadc485e77a05f2bc4a23a633bde354
Malanius <malanius@seznam.cz>
2018-03-01 20:11:59 +0100

    Add HashSet solution

---
+++ 04_DataStructures/036_HashSet/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        //Write your code here
+        HashSet<String> pairs = new HashSet<>();
+
+        for (int i = 0; i < pair_left.length; i++) {
+            String combination = pair_left[i] + " " + pair_right[i];
+            pairs.add(combination);
+            System.out.println(pairs.size());
+        }
+    }
+}
